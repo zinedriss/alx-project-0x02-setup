d5ab35b9b7d0c979a6c56c730e93469257e847e9
idriss <ssenuoy.issiurd@gmail.com>
2025-07-27 23:29:15 +0100

    add foldders

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -1,8 +1,8 @@
 // pages/users.tsx
-import type { NextPage, GetStaticProps } from 'next';
-import Header from '@/components/layout/Header';
-import UserCard from '@/components/common/UserCard';
-import { type UserProps } from '@/interfaces';
+import type { NextPage, GetStaticPropsContext, GetStaticPropsResult } from 'next';
+import Header from '../components/layout/Header';
+import UserCard from '../components/common/UserCard';
+import { type UserProps } from '../interfaces';
 import styles from '../styles/Users.module.css';
 
 interface UsersPageProps {
@@ -34,9 +34,9 @@ const UsersPage: NextPage<UsersPageProps> = ({ users }) => {
 /**
  * This function, getStaticProps, fetches data at build time.
  * It runs on the server-side and ensures the user data is available when the page is rendered.
- * @returns {Promise<{ props: UsersPageProps }>} An object containing the props for the page.
+ * @returns {Promise<GetStaticPropsResult<UsersPageProps>>} An object containing the props for the page.
  */
-export const getStaticProps: GetStaticProps<UsersPageProps> = async () => {
+export async function getStaticProps(): Promise<GetStaticPropsResult<UsersPageProps>> {
   try {
     const response = await fetch('https://jsonplaceholder.typicode.com/users');
     if (!response.ok) {
@@ -57,7 +57,7 @@ export const getStaticProps: GetStaticProps<UsersPageProps> = async () => {
       },
     };
   }
-};
+}
 
 /*
   You can create a file named 'Users.module.css' in the 'styles' directory
