dc78bc615fd1f44498ffd4008841330ef0158e0a
idriss <ssenuoy.issiurd@gmail.com>
2025-07-27 22:46:06 +0100

    add foldders

---
+++ alx-project-0x02/components/layout/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="w-full bg-blue-600 p-4 text-white shadow-md">
+      <nav className="container mx-auto">
+        <h1 className="text-2xl font-bold">ALX Project 2</h1>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
